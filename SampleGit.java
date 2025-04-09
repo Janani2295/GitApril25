b1293d3406d22e836e9052c22facd05859fde206
Keerthi-space <keerthispace98@gmail.com>
2025-04-09 15:29:14 +0530

    addeded 2 methods- funt, nonfunc

--- SampleGit.java
+++ SampleGit.java
@@ -8,5 +8,15 @@ public class SampleGit {
 	private void b_method() {
 		// TODO Auto-generated method stub
 
+	} //Tester2
+	
+	private void functionaltesting() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void nonfunctionaltesting() {
+		// TODO Auto-generated method stub
+
 	}
 }
